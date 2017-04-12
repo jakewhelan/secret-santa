b562dc942174a330acf6c266e5e6bab9c65a0106
Jake Whelan <jakewhelan22@gmail.com>
2017-04-12 21:24:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Secret Santa
+
+## Description
+Angular 2 & Node.js client/server "secret santa" generator prototype
+
+## Usage
+Run `node app.js` in /server to start server, then `ng serve --prod --aot` in /client to start client
---
+++ server/app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var fs = require("fs");
+
+var app = express();
+var port = 3000;
+app.set("port", port);
+
+app.get("/api/users/", function(request, response) {
+  fs.readFile('./data/usersw.json', 'utf8', function (error, data) {
+    if(error) {
+      return console.error("Error reading file: " + error);
+    }
+    response.json(JSON.parse(data));
+  });
+});
+
+app.listen("3000", function() {
+  console.log("listening on 3000");
+});
